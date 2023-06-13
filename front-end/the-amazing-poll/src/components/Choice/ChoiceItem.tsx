f1fb31fa8749c6dea94bd03ac93373fb4267ac01
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-14 00:53:27 +0300

    Finished the Preview component.

--- front-end/the-amazing-poll/src/components/Choice/ChoiceItem.tsx
+++ front-end/the-amazing-poll/src/components/Choice/ChoiceItem.tsx
@@ -4,10 +4,10 @@ import Choice from '../../models/Choice'
 
 const ChoiceItem: React.FC<Choice> = (props) => {
     return (
-        <div className={styles.choiceContainer}>
+        <li className={styles.choiceContainer}>
             <h3 className={styles.title}>{props.title}</h3>
             <h4 className={styles.percentage}>{`${props.percentage}%`}</h4>
-        </div>
+        </li>
     );
 }
 
