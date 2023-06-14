981d59d52903b27572c17662308ecef3dfae4cbc
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-15 01:21:35 +0300

    Added basic form styles.

--- front-end/the-amazing-poll/src/components/NewPollForm/NewPollForm.tsx
+++ front-end/the-amazing-poll/src/components/NewPollForm/NewPollForm.tsx
@@ -5,6 +5,7 @@ import TextInput from '../TextInput/TextInput';
 const NewPollForm = () => {
     return (
         <div className={styles.formContainer}>
+            <h1 className={styles.title}>Create new Poll</h1>
             <form action="">
                 <TextInput title='Question' />
                 <TextInput title='Option' />
