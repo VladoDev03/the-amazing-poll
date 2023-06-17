fa35cb5cfff049b0a43932a265fa4638a122b9af
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-17 17:37:25 +0300

    Configured body-parser on the back-end.

--- back-end/the-amazing-poll/src/app.ts
+++ back-end/the-amazing-poll/src/app.ts
@@ -1,8 +1,11 @@
 import express, {Express, Request, Response} from "express";
+import bodyParser from "body-parser";
 
 const port = 8080;
 
 const app: Express = express();
+
+app.use(bodyParser.json()); // application/json
 
 app.get('', (req: Request, res: Response) => {
     res.send("The Amazing Poll");
