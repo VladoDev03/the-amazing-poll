94cca88a8fafa267e69e0a90b6d2246948775a2d
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-17 13:36:09 +0300

    Added back preview and navbar components.

---
+++ front-end/the-amazing-poll/src/components/preview/Preview.tsx
@@ -0,0 +1,24 @@
+import styles from './Preview.module.scss'
+
+import ChoicesList from '../choice-list/ChoicesList';
+
+import Choice from '../../models/Choice';
+
+const Preview = () => {
+    const items: Choice[] = [
+        new Choice("C#", 35),
+        new Choice("JS", 25),
+        new Choice("TS", 20),
+        new Choice("C++", 15),
+        new Choice("Kotlin", 5)
+    ]
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Languages</h1>
+            <ChoicesList items={items} />
+        </div>
+    );
+}
+
+export default Preview;
