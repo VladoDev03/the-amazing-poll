0779c1e75b752238f6caf43adc68fe5dca142663
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-17 17:53:10 +0300

    Configured CORS policies.

--- back-end/the-amazing-poll/src/app.ts
+++ back-end/the-amazing-poll/src/app.ts
@@ -1,11 +1,18 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
 const port = 8080;
 
 const app: Express = express();
 
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.get('', (req: Request, res: Response) => {
     res.send("The Amazing Poll");
