3a06fde91cec8ae8808c24dedf6911a816139877
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-17 14:16:50 +0300

    Added Navbar component.

--- front-end/the-amazing-poll/src/App.tsx
+++ front-end/the-amazing-poll/src/App.tsx
@@ -5,14 +5,18 @@ import { Route, Routes } from 'react-router-dom';
 import LoginForm from './components/login-form/LoginForm';
 import RegisterForm from './components/register-form/RegisterForm';
 import NewPoll from './components/new-poll/NewPoll';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
     return (
-        <Routes>
-            <Route path='/new-poll' element={<NewPoll />}></Route>
-            <Route path='/login' element={<LoginForm />}></Route>
-            <Route path='/register' element={<RegisterForm />}></Route>
-        </Routes>
+        <div className='App'>
+            <Navbar />
+            <Routes>
+                <Route path='/new-poll' element={<NewPoll />}></Route>
+                <Route path='/login' element={<LoginForm />}></Route>
+                <Route path='/register' element={<RegisterForm />}></Route>
+            </Routes>
+        </div>
     );
 }
 
--- front-end/the-amazing-poll/src/components/navbar/Navbar.tsx
+++ front-end/the-amazing-poll/src/components/navbar/Navbar.tsx
@@ -1,7 +1,48 @@
 import styles from './Navbar.module.scss'
 
+import { useState } from 'react'
+
+import { NavLink, useNavigate } from 'react-router-dom'
+
 const Navbar = () => {
-    return (<h1 className={styles.title}>Navbar</h1>);
+    const [isMenuActive, setIsMenuActive] = useState(false)
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        setIsMenuActive(oldState => !oldState)
+    }
+
+    const takeMeHome = () => {
+        navigate('/')
+    }
+
+    const setStyle = ({ isActive }: ActiveLink) => {
+        return isActive ? styles.activeLink : ''
+    }
+
+    return (
+        <div className={styles.navbarContainer}>
+            <nav className={styles.navbar}>
+                <div onClick={takeMeHome}><h1 className={styles.brandTitle}>The Amazing Poll</h1></div>
+                <a href='#/' className={styles.toggleButton} onClick={handleClick}>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                </a>
+                <div className={`${styles.navbarLinks} ${isMenuActive ? styles.active : ''}`}>
+                    <ul>
+                        <li><NavLink className={setStyle} to="/login">Login</NavLink></li>
+                        <li><NavLink className={setStyle} to="/register">Register</NavLink></li>
+                        <li><NavLink className={setStyle} to="/new-poll">New Poll</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+interface ActiveLink {
+    isActive: boolean;
 }
 
 export default Navbar;
