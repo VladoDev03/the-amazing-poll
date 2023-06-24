23a9c6e961b9e46a6302df8da6ffbcdcc66013c7
Vladimir Stoyanov <vladsto101@gmail.com>
2023-06-24 20:23:53 +0300

    Added getting by id on the back-end.

--- back-end/the-amazing-poll/src/controllers/PollController.ts
+++ back-end/the-amazing-poll/src/controllers/PollController.ts
@@ -3,6 +3,16 @@ import mongoose from 'mongoose';
 import Poll from '../models/Poll';
 
 // TODO: use async await
+const getPoll = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    return Poll
+        .findById(id)
+        .then(poll => res.status(200).json({ poll }))
+        .catch(error => res.status(500).json({ error }));
+}
+
+// TODO: use async await
 const createPoll = (req: Request, res: Response, next: NextFunction) => {
     const title = req.body.title;
     const choices = req.body.choices;
@@ -19,4 +29,4 @@ const createPoll = (req: Request, res: Response, next: NextFunction) => {
         .catch(error => res.status(500).json({ error }));
 }
 
-export default { createPoll };
+export default { getPoll, createPoll };
--- back-end/the-amazing-poll/src/routes/PollRouter.ts
+++ back-end/the-amazing-poll/src/routes/PollRouter.ts
@@ -3,6 +3,7 @@ import controller from '../controllers/PollController';
 
 const router = express.Router();
 
+router.get('/get/:id', controller.getPoll);
 router.post('/create', controller.createPoll);
 
 export default router;
